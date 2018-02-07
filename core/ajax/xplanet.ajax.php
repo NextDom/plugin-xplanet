ae60e03cabb8204abd8edc536e3cdca5aac53428
ColonelMoutarde <sanchezluc@gmail.com>
2018-02-07 11:40:02 +0100

    remove ?> and global namespace

--- core/ajax/xplanet.ajax.php
+++ core/ajax/xplanet.ajax.php
@@ -38,9 +38,9 @@ try {
         ajax::success();
     }
 
-    throw new Exception(__('Aucune methode correspondante à : ', __FILE__) . init('action'));
+    throw new \Exception(__('Aucune methode correspondante à : ', __FILE__) . init('action'));
     /*     * *********Catch exeption*************** */
-} catch (Exception $e) {
+} catch (\Exception $e) {
     ajax::error(displayExeption($e), $e->getCode());
 }
-?>
+
